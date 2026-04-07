fe552ee0a51362fed88702610b4e9aa7d4c6ee3a
Sukh-Ochir927 <batjargalsukhochir27@gmail.com>
2026-04-07 14:45:53 +0800

    fix deployment

--- server/src/controller/orders/get-orders.ts
+++ server/src/controller/orders/get-orders.ts
@@ -2,7 +2,15 @@ import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
 
 export const getOrders = async (req: Request, res: Response) => {
-  const orders = await prisma.foodOrder.findMany();
-
+  const orders = await prisma.foodOrder.findMany({
+    include: {
+      userOrder: true,
+      FoodOrderItems: {
+        include: {
+          food: true,
+        },
+      },
+    },
+  });
   res.json(orders);
 };
